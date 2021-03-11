afc9ccbea6be20a31e090ddafbe5a22481395511
edmar <edmar.djou@gmail.com>
2021-03-11 15:42:29 -0300

    em desenvolvimento

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Navigator } from './styles';
 import { Link } from 'react-router-dom'
 
 function Menu() {
+  const [clicked, setClicked] = useState(false)
+
+  useEffect(()=>[clicked])
+
+  function isClicked(){
+    setClicked(!clicked)
+  }
+
+
+  console.warn(clicked)
   return (
     <Container>
-      <Link to="/">
+      <Link to="/" style={{ textDecoration: 'none' }} onClick={()=> isClicked()}>
         <Navigator>Home</Navigator>
       </Link>
-      <Link to="/newcomics">
+      <Link to="/newcomics" style={{ textDecoration: 'none' }}>
         <Navigator>New Comics</Navigator>
       </Link>
-      <Link to="/movies">
+      <Link to="/movies" style={{ textDecoration: 'none' }}>
         <Navigator>Movies</Navigator>
       </Link>
     </Container>
